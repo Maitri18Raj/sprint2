3265f5544b7443067b45dcabc5d3899d61263c7d
Maitri <maitriraj18@gmail.com>
2020-05-10 23:29:54 +0530

    Third Commit.

--- question-mgt/src/main/java/org/cap/controller/QuestionRestController.java
+++ question-mgt/src/main/java/org/cap/controller/QuestionRestController.java
@@ -2,6 +2,7 @@ package org.cap.controller;
 
 import java.math.BigInteger;
 
+
 import javax.validation.Valid;
 
 import org.cap.dto.QuestionDto;
@@ -12,6 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,25 +23,34 @@ public class QuestionRestController {
 	private IQuestionService service;
 	
 	 @PostMapping("/add")
-	    public ResponseEntity<Questions>createTest(@RequestBody @Valid QuestionDto QuestionDto){
+	    public ResponseEntity<Questions>addQuestion(@RequestBody @Valid QuestionDto QuestionDto){
 	       Questions question = convert(QuestionDto);
 	       BigInteger id = test.getTestId();
-	       question=service.addQuestion(testId,question);
+	       question=service.addQuestions(id,question);
 	        ResponseEntity<Questions>response=new ResponseEntity<>(question, HttpStatus.OK);
 	        return response;
 	   }
 	 
 	 @GetMapping("/remove")
-		public ResponseEntity<Questions> deleteTest(@RequestBody @Valid QuestionDto QuestionDto) {	
+		public ResponseEntity<Questions> removeQuestion(@RequestBody @Valid QuestionDto QuestionDto) {	
 		 Questions question = convert(QuestionDto);
 		 BigInteger id = test.getTestId();	
-		 test = service.deleteQuestion(id);
+		 question = service.deleteQuestions(id,question);
 		 ResponseEntity<Questions>response=new ResponseEntity<>(question, HttpStatus.OK);
 			return response;
+	 }
 	 
+	 @PutMapping("/update")
+	     public ResponseEntity<Questions> updateQuestion(@RequestBody @Valid QuestionDto QuestionDto) {
+	     Questions question = convert(QuestionDto);
+		 BigInteger id = test.getTestId();	
+		 question = service.updateQuestions(id, question);
+		 ResponseEntity<Questions> response = new ResponseEntity<>(question, HttpStatus.OK);
+		 return response;		
+	 }
 
 	
-	public Questions  convert(QuestionDto dto) {
+	public Questions convert(QuestionDto dto) {
 		Questions question = new Questions();
 		question.setQuestionId(dto.getQuestionId());
 		question.setQuestionTitle(dto.getQuestionTitle());
